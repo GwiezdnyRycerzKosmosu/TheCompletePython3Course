9a3442f35acdf3d9db36e0f0c8bbd11bd6effa9f
Mateusz <kamykcool@gmail.com>
2021-02-16 23:46:37 +0100

    Commit Wife PC

--- main.py
+++ main.py
@@ -73,3 +73,23 @@ string2= string + "6 2222 + 888"
 print(string2)
 new3= re.sub('[^0-9]','',string2)
 print(new3)
+
+#breaking the while loop
+import random
+
+playerhp = 260
+enemyatkl = 60
+enemyatkh = 80
+
+while playerhp > 0:
+    dmg = random.randrange(enemyatkl, enemyatkh)
+    playerhp = playerhp - dmg
+
+    if playerhp <= 30:
+        playerhp =30
+
+    print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
+
+    if playerhp == 30:
+        print("You have low health. You have been teleported to the nearest inn")
+        break
